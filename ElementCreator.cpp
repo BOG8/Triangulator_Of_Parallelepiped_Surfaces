f5ed29ef286c77962ee45279ffcda652c1fca5f9
Олег Большаков < >
2017-02-07 16:53:26 +0300

    Added templateNumberOne function

--- ElementCreator.cpp
+++ ElementCreator.cpp
@@ -36,3 +36,50 @@ void ElementCreator::setArrayOfNodes(Node ***tempArrayOfNodes) {
 		}
 	}
 }
+
+void ElementCreator::templateNumberOne(int xStep, int yStep, int zStep) {
+	Element elementOne;
+	elementOne.k = 0;	// Заглушка
+	elementOne.nodesNumbers.push_back(arrayOfNodes[xStep][yStep][zStep].number);
+	elementOne.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep][zStep].number);
+	elementOne.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep + 1][zStep].number);
+	elementOne.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep][zStep + 1].number);
+	elements.push_back(elementOne);
+	elementOne.nodesNumbers.clear();
+
+	Element elementTwo;
+	elementTwo.k = 0;	// Заглушка
+	elementTwo.nodesNumbers.push_back(arrayOfNodes[xStep][yStep][zStep].number);
+	elementTwo.nodesNumbers.push_back(arrayOfNodes[xStep][yStep + 1][zStep].number);
+	elementTwo.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep + 1][zStep].number);
+	elementTwo.nodesNumbers.push_back(arrayOfNodes[xStep][yStep + 1][zStep + 1].number);
+	elements.push_back(elementTwo);
+	elementTwo.nodesNumbers.clear();
+
+	Element elementThree;
+	elementThree.k = 0;	// Заглушка
+	elementThree.nodesNumbers.push_back(arrayOfNodes[xStep][yStep][zStep].number);
+	elementThree.nodesNumbers.push_back(arrayOfNodes[xStep][yStep][zStep + 1].number);
+	elementThree.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep][zStep + 1].number);
+	elementThree.nodesNumbers.push_back(arrayOfNodes[xStep][yStep + 1][zStep + 1].number);
+	elements.push_back(elementThree);
+	elementThree.nodesNumbers.clear();
+
+	Element elementFour;
+	elementFour.k = 0;	// Заглушка
+	elementFour.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep + 1][zStep].number);
+	elementFour.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep][zStep + 1].number);
+	elementFour.nodesNumbers.push_back(arrayOfNodes[xStep][yStep + 1][zStep + 1].number);
+	elementFour.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep + 1][zStep + 1].number);
+	elements.push_back(elementFour);
+	elementFour.nodesNumbers.clear();
+
+	Element elementFive;
+	elementFive.k = 0;	// Заглушка
+	elementFive.nodesNumbers.push_back(arrayOfNodes[xStep][yStep][zStep].number);
+	elementFive.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep + 1][zStep].number);
+	elementFive.nodesNumbers.push_back(arrayOfNodes[xStep + 1][yStep][zStep + 1].number);
+	elementFive.nodesNumbers.push_back(arrayOfNodes[xStep][yStep + 1][zStep + 1].number);
+	elements.push_back(elementFive);
+	elementFive.nodesNumbers.clear();
+}
--- ElementCreator.h
+++ ElementCreator.h
@@ -1,8 +1,11 @@
 #include "Node.h"
+#include "Element.h"
+#include <vector>
 
 class ElementCreator {
 	int xStepsNumber, yStepsNumber, zStepsNumber;
 	Node ***arrayOfNodes;
+	vector<Element> elements;
 
 public:
 	ElementCreator();
@@ -10,7 +13,7 @@ public:
 
 	void setStepsNumbers(int xStepsNumber, int yStepsNumbers, int zStepsNumbers);
 	void setArrayOfNodes(Node ***tempArrayOfNodes);
-
+	
 private:
-
+	void templateNumberOne(int xStep, int yStep, int zStep);
 };
